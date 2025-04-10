c38c8b932a9a009aa71d2cbe5300f01a329f2446
Prashant Kumar <2018187@iiitdmj.ac.in>
2025-04-10 13:46:47 +0530

    statement in JDBC

--- SimpleJDBCProject/src/com/prashant/simpleJDBC/Main.java
+++ SimpleJDBCProject/src/com/prashant/simpleJDBC/Main.java
@@ -1,6 +1,8 @@
 package com.prashant.simpleJDBC;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
@@ -13,6 +15,16 @@ public class Main {
 		try (Connection connection = DBConfigurationService.getConnection(); Scanner sc = new Scanner(System.in);) {
 			System.out.println("Welcome to School Management System");
 
+			// select using create statement.
+			Statement statement = connection.createStatement();
+			String query = "select * from student";
+			ResultSet resultSet = statement.executeQuery(query);
+			while (resultSet.next()){
+				System.out.println("id : "+resultSet.getInt("id"));
+				System.out.println("name : "+resultSet.getString("name"));
+				System.out.println("age : "+resultSet.getInt("age"));
+				System.out.println("marks : "+resultSet.getDouble("marks"));
+			}
 		}
 		catch(Exception ex){
 			ex.printStackTrace();
